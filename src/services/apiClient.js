af43ca7d2243993e26829a79ebe165e9eb320d46
Samriddhi Shrivastava <samriddhishrivastav19@gmail.com>
2026-07-17 02:07:58 +0530

    update backend api url

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://herspherebackend.onrender.com/api",
+    baseURL: "https://hersphere-backend.onrender.com/api",
 });
 
 export default API;
