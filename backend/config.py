a6fe14f03040181f7f67be48a641df985144d690
Samriddhi Shrivastava <samriddhishrivastav19@gmail.com>
2026-07-18 13:55:26 +0530

    fix render python and postgres setup

--- backend/config.py
+++ backend/config.py
@@ -4,12 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Fix Render postgres URL format
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+
 class Config:
 
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
-    )
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
